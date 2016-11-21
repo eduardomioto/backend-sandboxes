fde4d3611c7afd938fc829731112cf6702648593
Eduardo Mioto <eduardo.mioto@ge.com>
2016-11-21 15:42:46 -0200

    Java - Spring Batch Updates

---
+++ java-sandbox/spring-batch/src/main/java/br/com/mioto/sandboxes/Main.java
@@ -0,0 +1,7 @@
+package br.com.mioto.sandboxes;
+
+public class Main {
+	public static void main(String [] args) {
+		//System.exit(SpringApplication.exit(SpringApplication.run(BatchConfiguration.class, args)));
+	}
+}
