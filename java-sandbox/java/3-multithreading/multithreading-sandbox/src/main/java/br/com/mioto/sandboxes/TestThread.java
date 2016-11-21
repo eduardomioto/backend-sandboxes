e2a6a9e94ad45c13498e2086bc2f649905327d4d
Eduardo Mioto <eduardo.mioto@gmail.com>
2016-11-21 17:36:51 -0200

    Java - Multithreading Updates

---
+++ java-sandbox/java/3-multithreading/multithreading-sandbox/src/main/java/br/com/mioto/sandboxes/TestThread.java
@@ -0,0 +1,12 @@
+package br.com.mioto.sandboxes;
+
+public class TestThread {
+
+	   public static void main(String args[]) {
+	      RunnableDemo R1 = new RunnableDemo( "Thread-1");
+	      R1.start();
+	      
+	      RunnableDemo R2 = new RunnableDemo( "Thread-2");
+	      R2.start();
+	   }   
+}
