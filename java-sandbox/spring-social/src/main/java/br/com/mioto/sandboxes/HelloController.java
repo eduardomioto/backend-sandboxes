e3b3eb5140d57548033ce8e13c11ab6769a7c5eb
Eduardo Mioto <eduardo.mioto@ge.com>
2016-11-21 15:42:23 -0200

    Java - Spring Social Updates

---
+++ java-sandbox/spring-social/src/main/java/br/com/mioto/sandboxes/HelloController.java
@@ -0,0 +1,36 @@
+package br.com.mioto.sandboxes;
+
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+    private Facebook facebook;
+    private ConnectionRepository connectionRepository;
+
+    public HelloController(Facebook facebook, ConnectionRepository connectionRepository) {
+        this.facebook = facebook;
+        this.connectionRepository = connectionRepository;
+    }
+
+    @GetMapping
+    public String helloFacebook(Model model) {
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return "redirect:/connect/facebook";
+        }
+
+        model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
+        PagedList<Post> feed = facebook.feedOperations().getFeed();
+        model.addAttribute("feed", feed);
+        return "hello";
+    }
+
+}
