d9f19802ae99f70135126fb6cffb912efae20717
Eduardo Mioto <eduardo.mioto@ge.com>
2016-11-21 23:08:14 -0200

    Python - Adding Files

---
+++ python-sandbox/getAllLinks.py
@@ -0,0 +1,13 @@
+import urllib2
+import re
+
+#connect to a URL
+website = urllib2.urlopen(url)
+
+#read html code
+html = website.read()
+
+#use re.findall to get all the links
+links = re.findall('"((http|ftp)s?://.*?)"', html)
+
+print links
---
+++ python-sandbox/sendMail.py
@@ -0,0 +1,32 @@
+"""
+sendemail(from_addr    = 'python@RC.net', 
+          to_addr_list = ['RC@gmail.com'],
+          cc_addr_list = ['RC@xx.co.uk'], 
+          subject      = 'Howdy', 
+          message      = 'Howdy from a python function', 
+          login        = 'pythonuser', 
+          password     = 'XXXXX')
+"""
+
+import smtplib
+ 
+def sendemail(from_addr, to_addr_list, cc_addr_list,
+              subject, message,
+              login, password,
+              smtpserver='smtp.gmail.com:587'):
+    header  = 'From: %s
+' % from_addr
+    header += 'To: %s
+' % ','.join(to_addr_list)
+    header += 'Cc: %s
+' % ','.join(cc_addr_list)
+    header += 'Subject: %s
+
+' % subject
+    message = header + message
+ 
+    server = smtplib.SMTP(smtpserver)
+    server.starttls()
+    server.login(login,password)
+    problems = server.sendmail(from_addr, to_addr_list, message)
+    server.quit()
