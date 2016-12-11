f5a4959b2f96836761380c1ebf8927ea5846d5f0
Eduardo Mioto <eduardo.mioto@ge.com>
2016-12-11 16:11:20 -0200

    Adding Travis CI yml and sh

--- build-script.sh
+++ build-script.sh
@@ -3,10 +3,11 @@ set -e
 components="axis cxf hibernate jackson java jersey jpa spring-batch spring-boot spring-boot-actuator spring-boot-websocket spring-cloud spring-data spring-jdbc spring-jms spring-mvc spring-security spring-social"
 
 cd "java-sandbox"
-for component in $components
-do
-    echo "Testing component: $component"
-    cd $component
-    mvn clean install
-done
+cd "spring-boot-actuator"
+#for component in $components
+#do
+#    echo "Testing component: $component"
+#    cd $component
+mvn clean install
+#done
 
