4add80ef936dc815f1d0dbbd07bd9b0832bf4527
Eduardo Mioto <eduardo.mioto@ge.com>
2016-12-11 16:30:32 -0200

    Updating Travis CI yml and sh

--- build-script.sh
+++ build-script.sh
@@ -1,6 +1,4 @@
 #/usr/bin/env/sh
-#set -e
-#components="axis cxf hibernate jackson java jersey jpa spring-batch spring-boot spring-boot-actuator spring-boot-websocket spring-cloud spring-data spring-jdbc spring-jms spring-mvc spring-security spring-social"
 components="spring-batch spring-boot spring-boot-actuator spring-cloud spring-data spring-jdbc spring-jms spring-mvc spring-security spring-social"
 cd "java-sandbox"
 for component in $components
