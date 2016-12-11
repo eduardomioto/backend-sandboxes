55157b2f1e9ceb0da34010896f7814ff1a833e93
Eduardo Mioto <eduardo.mioto@ge.com>
2016-12-11 16:23:25 -0200

    Updating Travis CI yml and sh

--- build-script.sh
+++ build-script.sh
@@ -1,4 +1,11 @@
 #/usr/bin/env/sh
+set -e
+#components="axis cxf hibernate jackson java jersey jpa spring-batch spring-boot spring-boot-actuator spring-boot-websocket spring-cloud spring-data spring-jdbc spring-jms spring-mvc spring-security spring-social"
+components="spring-batch spring-boot spring-boot-actuator spring-cloud spring-data spring-jdbc spring-jms spring-mvc spring-security spring-social"
 cd "java-sandbox"
-cd "spring-boot-actuator"
-mvn clean install
+for component in $components
+do
+    echo "Testing component: $component"
+    cd $component
+    mvn clean install
+done
